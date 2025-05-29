f931167017a565813734293f863702b814883ea3
maxime <maxime@alnitaka.com>
2025-05-30 00:34:26 +0200

    Initial Commit

---
+++ zenon/zenonApp.swift
@@ -0,0 +1,17 @@
+//
+//  zenonApp.swift
+//  zenon
+//
+//  Created by Maxime Tiger on 30/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct zenonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ zenonTests/zenonTests.swift
@@ -0,0 +1,16 @@
+//
+//  zenonTests.swift
+//  zenonTests
+//
+//  Created by Maxime Tiger on 30/05/2025.
+//
+
+import Testing
+
+struct zenonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
